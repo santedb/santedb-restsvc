821f66223efb71afb2b2cff29ad31eccaecc0e70
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-01 15:58:15 +0630

    Added service info

--- SanteDB.Core.Model.AMI/Diagnostics/DiagnosticServiceInfo.cs
+++ SanteDB.Core.Model.AMI/Diagnostics/DiagnosticServiceInfo.cs
@@ -18,12 +18,26 @@
  * Date: 2018-11-20
  */
 using Newtonsoft.Json;
+using SanteDB.Core.Services;
 using System.Linq;
 using System.Reflection;
 using System.Xml.Serialization;
 
 namespace SanteDB.Core.Model.AMI.Diagnostics
 {
+
+    /// <summary>
+    /// Represets the type of service classes
+    /// </summary>
+    [XmlType(nameof(ServiceClass), Namespace = "http://santedb.org/ami/diagnostics")]
+    public enum ServiceClass
+    {
+        Daemon, 
+        Data,
+        Repository,
+        Passive
+    }
+
     /// <summary>
     /// Represents diagnostic service info
     /// </summary>
@@ -42,11 +56,20 @@ namespace SanteDB.Core.Model.AMI.Diagnostics
         /// </summary>
         public DiagnosticServiceInfo(object daemon)
         {
-            var dat = daemon?.GetType().GetTypeInfo().GetCustomAttributes().FirstOrDefault(a => a.GetType().Name == "DescriptionAttribute");
-            this.Description = dat?.GetType().GetRuntimeProperty("Description")?.GetValue(dat)?.ToString();
+            this.Description = daemon.GetType().GetTypeInfo().GetCustomAttribute<ServiceProviderAttribute>()?.Name;
             this.IsRunning = (bool)daemon.GetType().GetRuntimeProperty("IsRunning")?.GetValue(daemon);
             this.Type = daemon.GetType().FullName;
+            this.Class = daemon is IDaemonService ? ServiceClass.Daemon :
+                daemon is IDataPersistenceService ? ServiceClass.Data :
+                daemon.GetType().GetTypeInfo().ImplementedInterfaces.Any(o => o.Name.Contains("IRepositoryService")) ? ServiceClass.Repository :
+                ServiceClass.Passive;
         }
+
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        [XmlElement("class"), JsonProperty("class")]
+        public ServiceClass Class { get; set; }
 
         /// <summary>
         /// Gets or sets the description
