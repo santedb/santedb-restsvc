51c8d92b2580e7c1ab20772beaeda391e91826b7
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-08 10:11:46 +0700

    rev'd versions

--- SanteDB.Core.Model.RISI/Properties/AssemblyInfo.cs
+++ SanteDB.Core.Model.RISI/Properties/AssemblyInfo.cs
@@ -44,4 +44,4 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.10.0.*")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
