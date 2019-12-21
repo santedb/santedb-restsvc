bd16ee7c6b03ac9fbbf7d2fcab9434cb888722c9
Justin Fyfe <justin@fyfesoftware.ca>
2019-12-21 16:05:34 +0630

    Updated service info version

--- SanteDB.Core.Model.RISI/Properties/AssemblyInfo.cs
+++ SanteDB.Core.Model.RISI/Properties/AssemblyInfo.cs
@@ -45,4 +45,4 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.100.0*")]
 [assembly: AssemblyVersion("1.100.0.*")]
 [assembly: AssemblyFileVersion("1.100.0.0")]
-[assembly: AssemblyInformationalVersion("Halifax")]
+[assembly: AssemblyInformationalVersion("Iqaluit")]
