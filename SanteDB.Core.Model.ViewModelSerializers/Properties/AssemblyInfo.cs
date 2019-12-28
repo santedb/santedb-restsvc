35b2bf489d93834c6e73e89d616e1aee4c1689ad
Justin Fyfe <justin@fyfesoftware.ca>
2019-12-28 16:02:18 +0630

    Rev'd versions to handle refactor of IJobManager Service

--- SanteDB.Core.Model.ViewModelSerializers/Properties/AssemblyInfo.cs
+++ SanteDB.Core.Model.ViewModelSerializers/Properties/AssemblyInfo.cs
@@ -43,5 +43,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.28.*")]
-[assembly: AssemblyVersion("1.107.0.0")]
-[assembly: AssemblyFileVersion("1.107.0.0")]
+[assembly: AssemblyVersion("1.108.0.0")]
+[assembly: AssemblyFileVersion("1.108.0.0")]
