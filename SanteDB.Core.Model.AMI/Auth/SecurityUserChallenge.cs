bc13273209f744289fb33f33122091939233c75a
Justin Fyfe <justin@fyfesoftware.ca>
2020-03-15 21:38:01 -0400

    Added the security user challenge model class

---
+++ SanteDB.Core.Model.AMI/Auth/SecurityUserChallenge.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SanteDB.Core.Model.AMI.Auth
+{
+    /// <summary>
+    /// Represents security user information
+    /// </summary>
+    [XmlType(nameof(SecurityUserChallengeInfo), Namespace = "http://santedb.org/ami")]
+    [XmlRoot(nameof(SecurityUserChallengeInfo), Namespace = "http://santedb.org/ami")]
+    [JsonObject(nameof(SecurityUserChallengeInfo))]
+    public class SecurityUserChallengeInfo
+    {
+
+        /// <summary>
+        /// Gets or sets the key of the challenge
+        /// </summary>
+        [XmlElement("challenge"), JsonProperty("challenge")]
+        public Guid ChallengeKey { get; set; }
+
+        /// <summary>
+        /// The challenge response
+        /// </summary>
+        [XmlElement("response"), JsonProperty("response")]
+        public String ChallengeResponse { get; set; }
+
+    }
+}
