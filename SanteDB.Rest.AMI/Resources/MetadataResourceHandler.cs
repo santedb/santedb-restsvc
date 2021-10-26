7bf55a5ae4e0bcd35c417acec4848aa60f89940e
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-26 15:58:17 -0400

    Made record match

--- SanteDB.Rest.AMI/Resources/MetadataResourceHandler.cs
+++ SanteDB.Rest.AMI/Resources/MetadataResourceHandler.cs
@@ -1,27 +1,29 @@
 ﻿/*
  * Portions Copyright 2019-2021, Fyfe Software Inc. and the SanteSuite Contributors (See NOTICE)
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej (Justin Fyfe)
  * Date: 2021-8-5
  */
+
 using SanteDB.Core.Model;
 using SanteDB.Core.Model.Query;
 using SanteDB.Core.Security;
 using SanteDB.Core.Services;
 using SanteDB.Rest.Common.Attributes;
 using System.Collections.Generic;
+
 using SanteDB.Core.Services;
 
 namespace SanteDB.Rest.AMI.Resources
@@ -41,7 +43,7 @@ namespace SanteDB.Rest.AMI.Resources
         }
 
         /// <summary>
-        /// Create a resource 
+        /// Create a resource
         /// </summary>
         [Demand(PermissionPolicyIdentifiers.UnrestrictedMetadata)]
         public override object Create(object data, bool updateIfExists)
@@ -83,14 +85,6 @@ namespace SanteDB.Rest.AMI.Resources
         public override object Update(object data)
         {
             return base.Update(data);
-        }
-
-        /// <summary>
-        /// DI constructor
-        /// </summary>
-        /// <param name="localizationService"></param>
-        public MetadataResourceHandler(ILocalizationService localizationService) : base(localizationService)
-        {
         }
     }
 }
