d24c5bbdee4e00ecc59889e4d57949d986be21d7
Trevor Davis <trevor@trevordavis.me>
2022-10-13 13:05:01 -0400

    Adding support to add rules to the database. Adding support for ActiRelationship and ActParticipation

--- SanteDB.Rest.HDSI/Resources/RelationshipRuleChildResourceHandler.cs
+++ SanteDB.Rest.HDSI/Resources/RelationshipRuleChildResourceHandler.cs
@@ -1,6 +1,7 @@
 ﻿using SanteDB.Core.BusinessRules;
 using SanteDB.Core.Diagnostics;
 using SanteDB.Core.Interop;
+using SanteDB.Core.Model.Acts;
 using SanteDB.Core.Model.Entities;
 using SanteDB.Core.Model.Query;
 using SanteDB.Core.Services;
@@ -19,6 +20,8 @@ namespace SanteDB.Rest.HDSI.Resources
         readonly IRelationshipValidationProvider _Provider;
 
         static readonly Type s_EntityRelationshipType = typeof(EntityRelationship);
+        static readonly Type s_ActRelationshipType = typeof(ActRelationship);
+        static readonly Type s_ActParticipationType = typeof(ActParticipation);
 
 
         public RelationshipRuleChildResourceHandler(IRelationshipValidationProvider provider)
@@ -33,38 +36,122 @@ namespace SanteDB.Rest.HDSI.Resources
 
         public ChildObjectScopeBinding ScopeBinding => ChildObjectScopeBinding.Class;
 
-        public Type[] ParentTypes => new[] { s_EntityRelationshipType };
+        public Type[] ParentTypes => new[] { s_EntityRelationshipType, s_ActRelationshipType, s_ActParticipationType };
 
         public string Name => "_relationshipRule";
 
         public object Add(Type scopingType, object scopingKey, object item)
         {
-            throw new NotImplementedException();
+            if (item is RelationshipValidationRule rr)
+            {
+                IRelationshipValidationRule rule = null;
+
+                if (scopingType == s_EntityRelationshipType)
+                {
+                    rule = _Provider.AddValidRelationship<EntityRelationship>(rr.SourceClassKey, rr.TargetClassKey, rr.RelationshipTypeKey, rr.Description);
+                }
+                else if (scopingType == s_ActParticipationType)
+                {
+                    rule = _Provider.AddValidRelationship<ActParticipation>(rr.SourceClassKey, rr.TargetClassKey, rr.RelationshipTypeKey, rr.Description);
+                }
+                else if (scopingType == s_ActRelationshipType)
+                {
+                    rule = _Provider.AddValidRelationship<ActRelationship>(rr.SourceClassKey, rr.TargetClassKey, rr.RelationshipTypeKey, rr.Description);
+                }
+                else
+                {
+                    throw new NotImplementedException($"Support for {scopingType.FullName} is not available.");
+                }
+
+                if (null != rule)
+                {
+                    rr.Key = rule.Key;
+                    return rr;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                throw new FormatException("Unsupported body type.");
+            }
         }
 
         public object Get(Type scopingType, object scopingKey, object key)
         {
-            throw new NotImplementedException();
+            IRelationshipValidationRule relationship = null;
+
+            if (scopingType == s_EntityRelationshipType)
+            {
+                relationship = _Provider.GetRuleByKey<EntityRelationship>((Guid)key);
+            }
+            else if (scopingType == s_ActParticipationType)
+            {
+                relationship = _Provider.GetRuleByKey<ActParticipation>((Guid)key);
+            }
+            else if (scopingType == s_ActRelationshipType)
+            {
+                relationship = _Provider.GetRuleByKey<ActRelationship>((Guid)key);
+            }
+            else
+            {
+                throw new NotImplementedException($"Support for {scopingType.FullName} is not available.");
+            }
+
+            if (null == relationship)
+            {
+                return null;
+            }
+            else
+            {
+                return new RelationshipValidationRule
+                {
+                    Key = relationship.Key,
+                    SourceClassKey = relationship.SourceClassKey,
+                    RelationshipTypeKey = relationship.RelationshipTypeKey,
+                    TargetClassKey = relationship.TargetClassKey,
+                    Description = relationship.Description
+                };
+            }
         }
 
         public IQueryResultSet Query(Type scopingType, object scopingKey, NameValueCollection filter)
         {
+            IEnumerable<IRelationshipValidationRule> relationships = null;
+
             if (scopingType == s_EntityRelationshipType)
             {
-                var relationship = _Provider.GetValidRelationships<EntityRelationship>().Select(rel => new RelationshipValidationRule
+                relationships = _Provider.GetValidRelationships<EntityRelationship>();
+            }
+            else if (scopingType == s_ActParticipationType)
+            {
+                relationships = _Provider.GetValidRelationships<ActParticipation>();
+            }
+            else if (scopingType == s_ActRelationshipType)
+            {
+                relationships = _Provider.GetValidRelationships<ActRelationship>();
+            }
+            else
+            {
+                throw new NotImplementedException($"Support for {scopingType.FullName} is not available.");
+            }
+
+            if (null == relationships)
+            {
+                return new MemoryQueryResultSet<RelationshipValidationRule>();
+            }
+            else
+            {
+                return new MemoryQueryResultSet<RelationshipValidationRule>(relationships.Select(rel => new RelationshipValidationRule
                 {
                     Key = rel.Key,
                     SourceClassKey = rel.SourceClassKey,
                     RelationshipTypeKey = rel.RelationshipTypeKey,
                     TargetClassKey = rel.TargetClassKey,
                     Description = rel.Description
-                });
-
-                return new MemoryQueryResultSet<IRelationshipValidationRule>(relationship);
-            }
-            else
-            {
-                throw new NotImplementedException($"Support for {scopingType.FullName} is not available.");
+                }));
             }
         }
 
