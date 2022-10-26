dee7723f19726f6ff04f8e3e6221b9dedaf4a9b4
Trevor Davis <trevor@santesuite.com>
2022-10-26 07:20:18 -0400

    Renamed and moved OAuth project from santedb-server to santedb-restsvc

---
+++ SanteDB.Rest.OAuth/IAssetProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace SanteDB.Rest.OAuth
+{
+    /// <summary>
+    /// OAuth Asset Provider Interface
+    /// </summary>
+    public interface IAssetProvider
+    {
+        /// <summary>
+        /// Gets a stream for an asset from the asset provider.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="locale"></param>
+        /// <param name="bindingValues"></param>
+        /// <returns></returns>
+        (Stream content, string mimeType) GetAsset(string id, string locale, IDictionary<string, string> bindingValues);
+    }
+}
