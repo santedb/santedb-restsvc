484f7c1c9adc7e6f5c509cf54f5f494179d877fa
Justin Fyfe <justin@fyfesoftware.ca>
2024-01-29 15:45:49 -0500

    Added auditing information to the backup

--- SanteDB.Rest.AMI/Operation/BackupOperation.cs
+++ SanteDB.Rest.AMI/Operation/BackupOperation.cs
@@ -1,9 +1,17 @@
-﻿using SanteDB.Core.Data.Backup;
+﻿using RestSrvr;
+using SanteDB.Core.Data.Backup;
 using SanteDB.Core.Interop;
+using SanteDB.Core.Model.Audit;
 using SanteDB.Core.Model.Parameters;
+using SanteDB.Core.Security;
+using SanteDB.Core.Security.Audit;
+using SanteDB.Core.Security.Services;
 using SanteDB.Rest.Common;
+using SharpCompress;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SanteDB.Rest.AMI.Operation
@@ -13,13 +21,15 @@ namespace SanteDB.Rest.AMI.Operation
     /// </summary>
     public class BackupOperation : IApiChildOperation
     {
+        private readonly IAuditService m_auditService;
         private readonly IBackupService m_backupService;
 
         /// <summary>
         /// Restore backup 
         /// </summary>
-        public BackupOperation(IBackupService backupService)
+        public BackupOperation(IBackupService backupService, IAuditService auditService)
         {
+            this.m_auditService = auditService;
             this.m_backupService = backupService;    
         }
 
@@ -37,9 +47,46 @@ namespace SanteDB.Rest.AMI.Operation
         {
             if(scopingType == typeof(BackupMedia) && Enum.TryParse<BackupMedia>(scopingKey.ToString(), out var media))
             {
-                _ = parameters.TryGet("password", out string pass);
-                this.m_backupService.Backup(media, pass);
-                return null;
+                var audit = this.m_auditService.Audit()
+                   .WithAction(Core.Model.Audit.ActionType.Execute)
+                   .WithEventIdentifier(Core.Model.Audit.EventIdentifierType.Export)
+                   .WithEventType("CREATE_BACKUP", "http://santedb.org/conceptset/SecurityAuditCode#Rest", "Create Backup")
+                   .WithHttpInformation(RestOperationContext.Current.IncomingRequest)
+                   .WithLocalDestination()
+                   .WithPrincipal()
+                   .WithRemoteSource(RemoteEndpointUtil.Current.GetRemoteClient());
+
+                try
+                {
+                    _ = parameters.TryGet("password", out string pass);
+                    var label = this.m_backupService.Backup(media, pass);
+                    audit.WithOutcome(OutcomeIndicator.Success)
+                        .WithAuditableObjects(new AuditableObject()
+                        {
+                            IDTypeCode = AuditableObjectIdType.Custom,
+                            CustomIdTypeCode = new AuditCode("BACKUPSET", "http://santedb.org/model"),
+                            LifecycleType = AuditableObjectLifecycle.Creation,
+                            ObjectId = label.Label,
+                            ObjectData = label.Assets.Select(o => new ObjectDataExtension(o.Name, o.AssetClassId.ToByteArray())).ToList(),
+                            Role = AuditableObjectRole.JobStream,
+                            Type = AuditableObjectType.SystemObject
+                        }, new AuditableObject()
+                        {
+                            IDTypeCode = AuditableObjectIdType.Uri,
+                            ObjectId = $"media://{media}",
+                            Role = AuditableObjectRole.DataDestination
+                        });
+                    return null;
+                }
+                catch(Exception)
+                {
+                    audit.WithOutcome(OutcomeIndicator.EpicFail);
+                    throw;
+                }
+                finally
+                {
+                    audit.WithTimestamp().Send();
+                }
             }
             else
             {
--- SanteDB.Rest.AMI/Operation/RestoreBackupOperation.cs
+++ SanteDB.Rest.AMI/Operation/RestoreBackupOperation.cs
@@ -1,10 +1,15 @@
-﻿using SanteDB.Core.Data.Backup;
+﻿using RestSrvr;
+using SanteDB.Core.Data.Backup;
 using SanteDB.Core.Interop;
 using SanteDB.Core.Model.Parameters;
+using SanteDB.Core.Security.Audit;
+using SanteDB.Core.Security;
+using SanteDB.Core.Security.Services;
 using SanteDB.Rest.Common;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SanteDB.Core.Model.Audit;
 
 namespace SanteDB.Rest.AMI.Operation
 {
@@ -13,13 +18,15 @@ namespace SanteDB.Rest.AMI.Operation
     /// </summary>
     public class RestoreBackupOperation : IApiChildOperation
     {
+        private readonly IAuditService m_auditService;
         private readonly IBackupService m_backupService;
 
         /// <summary>
         /// Restore backup 
         /// </summary>
-        public RestoreBackupOperation(IBackupService backupService)
+        public RestoreBackupOperation(IBackupService backupService, IAuditService auditService)
         {
+            this.m_auditService = auditService;
             this.m_backupService = backupService;    
         }
 
@@ -37,9 +44,40 @@ namespace SanteDB.Rest.AMI.Operation
         {
             if(scopingType == typeof(BackupMedia) && Enum.TryParse<BackupMedia>(scopingKey.ToString(), out var media) && parameters.TryGet("label", out string label))
             {
-                _ = parameters.TryGet("password", out string password);
-                this.m_backupService.Restore(media, label, password);
-                return null;
+                var audit = this.m_auditService.Audit()
+                    .WithAction(Core.Model.Audit.ActionType.Execute)
+                    .WithEventIdentifier(Core.Model.Audit.EventIdentifierType.Import)
+                    .WithEventType("RESTORE_BACKUP", "http://santedb.org/conceptset/SecurityAuditCode#Rest", "Restore From Backup")
+                    .WithHttpInformation(RestOperationContext.Current.IncomingRequest)
+                    .WithLocalDestination()
+                    .WithPrincipal()
+                    .WithRemoteSource(RemoteEndpointUtil.Current.GetRemoteClient())
+                    .WithAuditableObjects(new AuditableObject()
+                    {
+                        IDTypeCode = AuditableObjectIdType.Custom,
+                        CustomIdTypeCode = new AuditCode("BACKUPSET", "http://santedb.org/model"),
+                        LifecycleType = AuditableObjectLifecycle.Access,
+                        ObjectId = label,
+                        Role = AuditableObjectRole.JobStream,
+                        Type = AuditableObjectType.SystemObject
+                    });
+
+                try
+                {
+                    _ = parameters.TryGet("password", out string password);
+                    this.m_backupService.Restore(media, label, password);
+                    audit.WithOutcome(Core.Model.Audit.OutcomeIndicator.Success);
+                    return null;
+                }
+                catch(Exception e)
+                {
+                    audit.WithOutcome(Core.Model.Audit.OutcomeIndicator.EpicFail);
+                    throw;
+                }
+                finally
+                {
+                    audit.WithTimestamp().Send();
+                }
             }
             else
             {
