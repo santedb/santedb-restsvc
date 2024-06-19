147aac23e7756f83154aaf89a9e1db98b6a951c8
Justin Fyfe <justin@fyfesoftware.ca>
2024-06-18 20:58:32 -0400

    Added the ConceptRelationshipResource handler to handle the maintenance of `ConceptRelationsip` instances

---
+++ SanteDB.Rest.HDSI/Resources/ConceptRelationshipResourceHandler.cs
@@ -0,0 +1,71 @@
+﻿using SanteDB.Core.Model.DataTypes;
+using SanteDB.Core.Model.Query;
+using SanteDB.Core.Security;
+using SanteDB.Core.Services;
+using SanteDB.Rest.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace SanteDB.Rest.HDSI.Resources
+{
+    /// <summary>
+    /// Concept relationship
+    /// </summary>
+    public class ConceptRelationshipResourceHandler : HdsiResourceHandlerBase<ConceptRelationship>
+    {
+        /// <summary>
+        /// DI constructor
+        /// </summary>
+        public ConceptRelationshipResourceHandler(ILocalizationService localizationService, IRepositoryService<ConceptRelationship> repositoryService, IResourceCheckoutService resourceCheckoutService, ISubscriptionExecutor subscriptionExecutor, IFreetextSearchService freetextSearchService = null) : base(localizationService, repositoryService, resourceCheckoutService, subscriptionExecutor, freetextSearchService)
+        {
+        }
+
+
+        /// <summary>
+        /// Create the specified object in the database
+        /// </summary>
+        [Demand(PermissionPolicyIdentifiers.AdministerConceptDictionary)]
+        public override Object Create(Object data, bool updateIfExists)
+        {
+            return base.Create(data, updateIfExists);
+        }
+
+        /// <summary>
+        /// Get the specified instance
+        /// </summary>
+        [Demand(PermissionPolicyIdentifiers.ReadMetadata)]
+        public override Object Get(object id, object versionId)
+        {
+            return base.Get(id, versionId);
+        }
+
+        /// <summary>
+        /// Obsolete the specified concept
+        /// </summary>
+        [Demand(PermissionPolicyIdentifiers.AdministerConceptDictionary)]
+        public override Object Delete(object key)
+        {
+            return base.Delete(key);
+        }
+
+        /// <summary>
+        /// Query the specified data
+        /// </summary>
+        [Demand(PermissionPolicyIdentifiers.ReadMetadata)]
+        public override IQueryResultSet Query(NameValueCollection queryParameters)
+        {
+            return base.Query(queryParameters);
+        }
+
+        /// <summary>
+        /// Update the specified data
+        /// </summary>
+        [Demand(PermissionPolicyIdentifiers.AdministerConceptDictionary)]
+        public override Object Update(Object data)
+        {
+            return base.Update(data);
+        }
+    }
+}
