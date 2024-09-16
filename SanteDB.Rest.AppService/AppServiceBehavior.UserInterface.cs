77b107fd8cb8a8ef1fba1d04ba6b222321656852
Justin Fyfe <justin@fyfesoftware.ca>
2024-09-16 10:15:01 -0400

    Updated the license headers to remove user/date of file (they were not accurate - as using file system times)

--- SanteDB.Rest.AppService/AppServiceBehavior.UserInterface.cs
+++ SanteDB.Rest.AppService/AppServiceBehavior.UserInterface.cs
@@ -15,8 +15,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  *
- * User: fyfej
- * Date: 2023-6-21
  */
 using RestSrvr;
 using SanteDB.Core.Applets.Model;
@@ -288,7 +286,7 @@ namespace SanteDB.Rest.AppService
                 {
                     if (o.Count() == 1)
                     {
-                        return (object)o.First();
+                        return (object)o.First().Value;
                     }
                     else
                     {
