138dbe4853fec869bb4dfbb69b464f6c54b82d1a
Jordan Webber <webberjo@hhsc.ca>
2025-03-17 15:10:11 -0400

    JIMS-747 Added Notification Template Resource Handler

---
+++ SanteDB.Rest.AMI/Resources/NotificationTemplateResourceHandler.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright (C) 2021 - 2025, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+using SanteDB.Core.Interop;
+using SanteDB.Core.Notifications;
+using SanteDB.Core.Services;
+
+namespace SanteDB.Rest.AMI.Resources
+{
+    /// <summary>
+    /// Represents a notification template resource handler
+    /// </summary>
+    public class NotificationTemplateResourceHandler : ResourceHandlerBase<NotificationTemplate>
+    {
+        /// <inheritdoc />
+        public NotificationTemplateResourceHandler(ILocalizationService localizationService, IRepositoryService<NotificationTemplate> repositoryService, ISubscriptionExecutor subscriptionExecutor, IFreetextSearchService freetextSearchService = null) : base(localizationService, repositoryService, subscriptionExecutor, freetextSearchService)
+        {
+        }
+
+        /// <inheritdoc />
+        public override ResourceCapabilityType Capabilities => ResourceCapabilityType.Create | ResourceCapabilityType.CreateOrUpdate | ResourceCapabilityType.Delete | ResourceCapabilityType.Get | ResourceCapabilityType.Search | ResourceCapabilityType.GetVersion;
+    }
+}
