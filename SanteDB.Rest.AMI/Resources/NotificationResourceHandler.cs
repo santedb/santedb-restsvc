5286a407fbd5bb0392133000a45550a9c0eaba22
Daria Ostroushko <ostroushko@HHSC.ca>
2025-04-01 09:49:19 -0400

    JIMS-704 Added Resource Handler for the Notification

---
+++ SanteDB.Rest.AMI/Resources/NotificationResourceHandler.cs
@@ -0,0 +1,17 @@
+﻿using SanteDB.Core.Interop;
+using SanteDB.Core.Notifications;
+using SanteDB.Core.Services;
+
+namespace SanteDB.Rest.AMI.Resources
+{
+    public class NotificationResourceHandler : ResourceHandlerBase<Notification>
+    {
+        /// <inheritdoc />
+        public NotificationResourceHandler(ILocalizationService localizationService, IRepositoryService<Notification> repositoryService, ISubscriptionExecutor subscriptionExecutor, IFreetextSearchService freetextSearchService = null) : base(localizationService, repositoryService, subscriptionExecutor, freetextSearchService)
+        {
+        }
+
+        /// <inheritdoc />
+        public override ResourceCapabilityType Capabilities => ResourceCapabilityType.Create | ResourceCapabilityType.CreateOrUpdate | ResourceCapabilityType.Delete | ResourceCapabilityType.Get | ResourceCapabilityType.Search | ResourceCapabilityType.Update;
+    }
+}
