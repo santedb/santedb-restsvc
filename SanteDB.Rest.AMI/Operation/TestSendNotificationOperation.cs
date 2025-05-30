dc653ad73cbd7a2b91902e26a8d32f1d2f8af225
Jordan Webber <webberjo@hhsc.ca>
2025-05-30 13:27:08 -0400

    Updated the documentation for the test sending notification and validate notification operations

--- SanteDB.Rest.AMI/Operation/TestSendNotificationOperation.cs
+++ SanteDB.Rest.AMI/Operation/TestSendNotificationOperation.cs
@@ -1,4 +1,22 @@
-﻿using SanteDB.Core.Interop;
+﻿/*
+ * Copyright (C) 2021 - 2025, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ */
+using SanteDB.Core.Interop;
 using SanteDB.Core.Model.Entities;
 using SanteDB.Core.Model.Parameters;
 using SanteDB.Core.Model.Query;
@@ -13,6 +31,9 @@ using System.Globalization;
 
 namespace SanteDB.Rest.AMI.Operation
 {
+    /// <summary>
+    /// Represents an operation to test sending a notification
+    /// </summary>
     public class TestSendNotificationOperation : IApiChildOperation
     {
         private readonly IRepositoryService<NotificationInstance> m_notificationInstanceRepositoryService;
@@ -22,6 +43,9 @@ namespace SanteDB.Rest.AMI.Operation
         private readonly INotificationTemplateRepository m_notificationTemplateRepository;
         private readonly INotificationService m_notificationService;
 
+        /// <summary>
+        /// DI constructor
+        /// </summary>
         public TestSendNotificationOperation(IRepositoryService<NotificationInstance> notificationInstanceRepositoryService, IRepositoryService<NotificationTemplate> notificationTemplateService, IRepositoryService<NotificationTemplateParameter> notificationTemplateParametersService, INotificationTemplateRepository notificationTemplateRepository, IEmailService emailService, INotificationService notificationService, IRepositoryService<EntityTelecomAddress> entityTelecomAddressRepositoryService)
         {
             this.m_notificationInstanceRepositoryService = notificationInstanceRepositoryService;
@@ -32,12 +56,16 @@ namespace SanteDB.Rest.AMI.Operation
             this.m_entityTelecomAddressRepositoryService = entityTelecomAddressRepositoryService;
         }
 
+        /// <inheritdoc/>
         public ChildObjectScopeBinding ScopeBinding => ChildObjectScopeBinding.Instance;
 
+        /// <inheritdoc/>
         public Type[] ParentTypes => new[] { typeof(NotificationInstance) };
 
+        /// <inheritdoc/>
         public string Name => "test-send-notification";
 
+        /// <inheritdoc/>
         public object Invoke(Type scopingType, object scopingKey, ParameterCollection parameters)
         {
             if (scopingKey == null)
--- SanteDB.Rest.AMI/Operation/ValidateNotificationOperation.cs
+++ SanteDB.Rest.AMI/Operation/ValidateNotificationOperation.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2021 - 2025, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
+ * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ */
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using SanteDB.Core.Interop;
@@ -9,6 +27,9 @@ using SanteDB.Rest.Common;
 
 namespace SanteDB.Rest.AMI.Operation
 {
+    /// <summary>
+    /// Represents an operation to validate a notification instance against its template
+    /// </summary>
     public class ValidateNotificationOperation : IApiChildOperation
     {
         private readonly IRepositoryService<NotificationInstance> m_notificationInstanceRepositoryService;
@@ -16,6 +37,9 @@ namespace SanteDB.Rest.AMI.Operation
         private readonly IRepositoryService<NotificationTemplateParameter> m_notificationTemplateParametersService;
         private readonly INotificationTemplateFiller m_notificationTemplateFiller;
 
+        /// <summary>
+        /// DI constructor
+        /// </summary>
         public ValidateNotificationOperation(IRepositoryService<NotificationInstance> notificationInstanceRepositoryService, IRepositoryService<NotificationTemplate> notificationTemplateService, IRepositoryService<NotificationTemplateParameter> notificationTemplateParametersService, INotificationTemplateRepository notificationTemplateRepository, INotificationTemplateFiller notificationTemplateFiller)
         {
             this.m_notificationInstanceRepositoryService = notificationInstanceRepositoryService;
@@ -24,12 +48,16 @@ namespace SanteDB.Rest.AMI.Operation
             this.m_notificationTemplateFiller = notificationTemplateFiller;
         }
 
+        /// <inheritdoc/>
         public ChildObjectScopeBinding ScopeBinding => ChildObjectScopeBinding.Instance;
 
+        /// <inheritdoc/>
         public Type[] ParentTypes => new[] { typeof(NotificationInstance) };
 
+        /// <inheritdoc/>
         public string Name => "validate-notification";
 
+        /// <inheritdoc/>
         public object Invoke(Type scopingType, object scopingKey, ParameterCollection parameters)
         {
             if (scopingKey == null)
